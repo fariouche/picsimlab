da8d0361cf25ede4d14d06e20d13b5db4f9faf3e
fariouche <fariouche@yahoo.fr>
2026-02-23 07:42:50 +0100

    fixed compilation issues and removed sudo from build script

--- bscripts/build_all_and_install.sh
+++ bscripts/build_all_and_install.sh
@@ -1,63 +1,76 @@
 #!/bin/bash 
 #show errors in color red 
-set -o pipefail
+set -eou pipefail
 cl()("$@" 2>&1>&3|sed $'s,.*,\e[31m&\e[m,'>&2)3>&1
 
+INSTDIR=$(pwd)/sysroot
+echo "INSTDIR=${INSTDIR}"
+mkdir -p ${INSTDIR}/usr/{lib,bin}
+PATH=${INSTDIR}/usr/bin:$PATH
+
 echo -e "\033[1;32m ---------------------- update and install packages ---------------------- \033[0m"
-cl sudo apt-get update
-cl sudo apt-get -y upgrade
-cl sudo apt-get -y install git doxygen gcc g++ make libwxgtk3.*-dev \
-libelf-dev freeglut3-dev cutecom gcc-avr avr-libc libopenal-dev libncurses-dev gtkwave \
-gedit cmake bison flex file pulseview libcurl4-openssl-dev
-cl sudo apt-get -y install linux-headers-`uname -r` dkms 
-cl mkdir build_all
+#cl sudo apt-get update
+#cl sudo apt-get -y upgrade
+#cl sudo apt-get -y install git doxygen gcc g++ make libwxgtk3.*-dev \
+#libelf-dev freeglut3-dev cutecom gcc-avr avr-libc libopenal-dev libncurses-dev gtkwave \
+#gedit cmake bison flex file pulseview libcurl4-openssl-dev
+#cl sudo apt-get -y install linux-headers-`uname -r` dkms 
+cl mkdir -p build_all
 cd build_all
 echo -e "\033[1;32m ---------------------- download deps -------------------------------------\033[0m"
+set +e
 git clone --depth=1 https://github.com/lcgamboa/picsim.git
 git clone --depth=1 https://github.com/lcgamboa/lxrad.git
 git clone --depth=1 https://github.com/lcgamboa/tty0tty.git
 git clone --depth=1 https://github.com/lcgamboa/simavr.git
 git clone --depth=1 https://github.com/lcgamboa/uCsim_picsimlab.git
-cl sudo apt-get -y install python3 libglib2.0-dev libpixman-1-dev libfdt-dev gpsim-dev gpsim \
-ninja-build meson libgcrypt-dev libslirp-dev
+#cl sudo apt-get -y install python3 libglib2.0-dev libpixman-1-dev libfdt-dev gpsim-dev gpsim \
+#ninja-build meson libgcrypt-dev libslirp-dev
 git clone --depth=1 --no-single-branch https://github.com/lcgamboa/qemu.git	
+set -e
 echo -e "\033[1;32m ---------------------- build and install picsim ------------------------- \033[0m"
 cd picsim
 cl git pull --no-rebase
 cl make clean;make -j$(nproc)
-cl sudo make install
+#cl sudo make install
+cl make install DESTDIR=${INSTDIR}
 cd ..
 echo -e "\033[1;32m ---------------------- build and install lxrad -------------------------- \033[0m"
 cd lxrad
 git pull --no-rebase
+patch -p1 < ../../lxrad.patch
 cl ./make_deps.sh
-cl ./configure --prefix=/usr
+cl ./configure --prefix=${INSTDIR}/usr
 cl make clean;make -j$(nproc)
-cl sudo make install
+#cl sudo make install
+cl make install DESTDIR=${INSTDIR}
 cd ..
 echo -e "\033[1;32m ---------------------- build and install tty0tty ------------------------ \033[0m"
 cd tty0tty/module
 git pull --no-rebase
-cl sudo ./dkms-install.sh
+#cl sudo ./dkms-install.sh
 #cl make clean;make -j$(nproc)
 #cl sudo make install
-sudo usermod -a -G dialout `whoami`
-cl sudo modprobe tty0tty
+#sudo usermod -a -G dialout `whoami`
+#cl sudo modprobe tty0tty
 cd ../../
 echo -e "\033[1;32m ---------------------- build and install simavr ------------------------- \033[0m"
 cd simavr
 git pull --no-rebase
 cl make clean;make build-simavr -j$(nproc) 
-cl sudo make install-simavr
+#cl sudo make install-simavr
+cl make install-simavr DESTDIR=${INSTDIR}
 cd ../
 echo -e "\033[1;32m ---------------------- build and install uCsim -------------------------- \033[0m"
 cd uCsim_picsimlab
 git pull --no-rebase
+patch -p1 < ../../uCsim_picsimlab.patch
 cl ./config_linux.sh
 cl make clean;make -j$(nproc)
 cd picsimlab
 cl make clean;make -j$(nproc)
-cl sudo make install
+#cl sudo make install
+cl make install DESTDIR=${INSTDIR}
 cd ../../
 echo -e "\033[1;32m ---------------------- build qemu  ---------------------- \033[0m"
 cd qemu
@@ -83,13 +96,14 @@ cd ..
 echo -e "\033[1;32m ---------------------- build and install picsimlab ---------------------- \033[0m"
 #git pull --no-rebase
 cd ../
-cl make clean;make -j$(nproc) 
-cl sudo make install
+cl make clean;make -j$(nproc) DESTDIR=${INSTDIR}
+#cl sudo make install
+cl make install DESTDIR=${INSTDIR}
 user=`whoami`
-cl sudo usermod -a -G dialout $user
+#cl sudo usermod -a -G dialout $user
 echo -e "\033[1;32m ---------------------- done! -------------------------------------------- \033[0m"
 echo -e "\033[1;32m logout your session and login to use serial \033[0m"
-sudo ldconfig
+#sudo ldconfig
 if xhost > /dev/null 2>&1 ; then
 cl picsimlab
 fi
